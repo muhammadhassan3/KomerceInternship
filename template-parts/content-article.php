05e1a0c9986c9c23aa3d9a4f9f435cb0098109ea
Lintang012004 <134138848+Lintang012004@users.noreply.github.com>
2024-08-26 11:57:48 +0700

    update

--- template-parts/content-article.php
+++ template-parts/content-article.php
@@ -1,6 +1,7 @@
 <div class="blog-post-card">
     <div class="blog-post-content">
         <h2 class="blog-post-title"><?php the_title();?></h2>
+        <div class="meta mb-3"><span class="blog-date"><?php the_date('d F Y'); ?></span></div> 
         <div class="blog-post-paragraph"><?php the_content();?></div>
     
     </div>
